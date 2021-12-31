b56e0724ea4db137a0769d55a9fa0067677624d3
catakeat <arsulescucatalin@yahoo.com>
2021-12-31 17:00:56 +0200

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Route } from "react-router-dom"
+import {  ToastContainer } from "react-toastify"
+import AddPost from "./components/AddContact";
+import EditContact from "./components/EditContact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import "./styles.css";
+
+
 
 const App = ()=> {
   return (
