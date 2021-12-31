2fb9524ae768ee0c126106160c99dae305483b08
catakeat <arsulescucatalin@yahoo.com>
2021-12-31 16:30:46 +0200

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
+import { createStore}  from "redux"
+import { Provider} from "react-redux"
+import {BrowserRouter as Router}  from "react-router-dom"
+/*import { contactReducer } from "./redux/reducers/contactReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
+const store = createStore(contactReducer, composeWithDevTools());
+*/
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    
+      <App />
+    
+ </Router>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
